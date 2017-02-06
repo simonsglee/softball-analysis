dc97d103c1e6edb604fc916b50a22ca6b3670f8d
Simon <simonsg.lee@gmail.com>
2017-02-05 19:24:54 -0500

    add R analysis and plot files

---
+++ regression_analysis.R
@@ -0,0 +1,39 @@
+### Korean Christian Softball League
+### Regression Analysis 2011-2016
+library(tidyverse)
+library(ggplot2)
+rm(list = ls())
+
+# load data
+kcsa <- read.csv("C:/Users/Simon/Documents/Rdirectory/kcsa/kcsa.csv")
+kcsa <- as_tibble(kcsa)
+
+# Make adjusted wins and losses for ties
+kcsa <- kcsa %>%
+    mutate(
+        adj.W = W + 0.5*T,
+        adj.L = L + 0.5*T,
+        adj.WPER = adj.W / GP,
+        adj.DIFF = DIFF / GP
+    ) %>%
+    select(Season:GP, adj.W, adj.L, adj.WPER, adj.DIFF, W:DIFF,-D)
+
+# Fit a linear regression model
+lm <- with(kcsa, lm(adj.WPER ~ adj.DIFF))
+summary(lm)
+
+# Filter 2016 season
+kcsa16 <- kcsa %>%
+    filter(Season == 2016)
+
+# Calculate expected win prediction from linear regression model
+kcsa16$Expected.W <- predict(lm, kcsa16)*10
+
+# Calculate column for over/under performing run differential
+kcsa16_diff <- kcsa16 %>%
+    mutate(Luck = adj.W - Expected.W) %>%
+    select(Season:adj.W, Expected.W, Luck, everything()) %>%
+    arrange(desc(Expected.W)) %>%
+    print(n = 32)
+
+
---
+++ regression_plots.R
@@ -0,0 +1,60 @@
+source("C:/Program Files/Git/softball-analysis/regression_analysis.R")
+
+label <- tibble(
+    adj.DIFF = Inf,
+    adj.WPER = -Inf,
+    label = paste(
+        "A tie counted as 0.5 Win and 0.5 Loss"
+    )
+)
+
+plot1 <- ggplot(data = kcsa, mapping = aes(x = adj.DIFF, y = adj.WPER)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE) +
+    scale_y_continuous(limits = c(0,1)) +
+    labs(
+        title = paste(
+            "KCSA Linear Regression Analysis (2011-2016)"
+        ),
+        y = paste("Adjusted Win %"),
+        x = paste("Adjusted Run Diff. per Game")
+    ) +
+    geom_text(
+        aes(label = label),
+        data = label,
+        hjust = "right",
+        vjust = -.5
+    )
+
+plot2 <- ggplot(data = kcsa16_diff, aes(x = reorder(TEAM, Luck), y = Luck)) +
+    geom_bar(stat = "identity", aes(fill = Luck)) +
+    coord_flip() +
+    labs(
+        title = ("KCSA 2016: Over/Underachievers"),
+        subtitle = ("Ordered by 'Luckiest' to 'Unluckiest'"),
+        x = "Teams",
+        y = "Over/Underachieving Margin"
+    ) +
+    scale_fill_gradientn(colours = topo.colors(2), "Over/Under")
+
+setwd("C:/Program Files/Git/softball-analysis")
+
+png(filename="regression_plot.png", 
+    type="cairo",
+    units="in", 
+    width=10, 
+    height=7, 
+    res=500)
+plot1
+dev.off()
+
+
+png(filename="luck_plot.png", 
+    type="cairo",
+    units="in", 
+    width=9, 
+    height=10, 
+    res=500)
+plot2
+dev.off()
+
